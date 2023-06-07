050da3f14a7adbc25655cf894fc34126d327ac3e
camillamakira <makiracamilla@gmail.com>
2023-06-07 12:33:53 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+        </header>
+      <main>List of Products.</main>
+      
+    </div>
+  );
+}
+
+export default App;
